fd400bb5fe4b8b9764c84bbc10c205a29c71a278
MrDragoNar <dragonarcissu@outlook.com>
2019-12-24 10:42:50 +0800

    origin version
    基于C#的CSV生成工具

---
+++ generateCSV/generateCSV/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace generateCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 80);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(517, 379);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 510);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "生成CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(517, 44);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "argsinfoV";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(545, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "使用方法：第一输入框可输入生成CSV文件名，下方输入框输入传感器ID，使用“/”标识符进行分割。";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Font = new System.Drawing.Font("宋体", 8.5F);
+            this.textBox2.Location = new System.Drawing.Point(12, 473);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(517, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 510);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 16);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Excel模式";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 532);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(89, 16);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "\"/\"分割模式";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 564);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "433Gate/CSV生成工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ generateCSV/generateCSV/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace generateCSV
+{
+    public partial class Form1 : Form
+    {
+        //默认配置文件在此处修改
+        public String ecof = ",2,ACC,10-5000,1600,,VEL,20-1000,400,4.5,DIS,20-500,400,,TMP,,,,abc";
+        public string[] sArray;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text = ecof;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   if (richTextBox1.Text.Length == 0)
+            {
+                MessageBox.Show("数据不能为空！");
+            }
+            else
+            {
+                //默认生成文件名在此处修改
+                string mFileName = textBox1.Text + ".csv";
+                string mData = textBox2.Text;
+                WriteCVS(mFileName, mData);
+            }
+        }
+
+        public void WriteCVS(string fileName, string data)
+        {
+            if (!File.Exists(fileName)) //当文件不存在时创建文件
+            {
+                //创建文件流(创建文件)
+                FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                //创建流写入对象，并绑定文件流
+                StreamWriter sw = new StreamWriter(fs,Encoding.GetEncoding("UTF-8"));
+                //实例化字符串流
+                StringBuilder sb = new StringBuilder();
+                //添加CSV文件标题（需要请修改此处！）
+                sb.Append("#ParameterTable").Append(",").Append("Never Change This Line").Append(",V3.0,,,,,,,,,").Append("\r\n");
+                sb.Append("SID,RawCycle,Signal_1,Bandwidth_1,Line_1,Alarm_1,Signal_2,Bandwidth_2,Line_2,Alarm_2,Signal_3,Bandwidth_3,Line_3,Alarm_3,Signal_4,Bandwidth_4,Line_4,Alarm_4,Info");
+                //将字符串流数据写入文件
+                sw.WriteLine(sb);
+                //刷新文件流
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            StreamWriter swd = new StreamWriter(fileName, true, Encoding.GetEncoding("UTF-8"));
+            StringBuilder sbd = new StringBuilder();
+            //将需要保存的数据添加到字符串流中
+            String str = richTextBox1.Text;
+            if (radioButton1.Checked)
+            {
+                sArray = str.Split(Environment.NewLine.ToCharArray());
+                for (int i = 0; i < sArray.Length - 1; i++)
+                {
+                    sbd.Append(sArray[i]).Append(data).Append("\r\n");
+                }
+            }
+            else 
+            {
+                sArray = str.Split('/');
+                for (int i = 0; i < sArray.Length ; i++)
+                {
+                    sbd.Append(sArray[i]).Append(data).Append("\r\n");
+                }
+            }
+            
+            swd.WriteLine(sbd);
+            swd.Flush();
+            swd.Close();
+            MessageBox.Show("生成成功，文件位于软件根目录。");
+        }
+    }
+}
+
